f7089db05b6cb65433d5af4ec918feb11b11f772
YusufLaidey <yusuflaideylee@gmail.com>
2022-06-09 14:41:42 +0100

    changed the hhtps link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-6b4a6.firebaseio.com/movies.json"
+        "https://react-http-fdf3e-default-rtdb.firebaseio.com/movies.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
